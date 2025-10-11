448a7e575b09189e23f7b4a84a87cd8ff924f1ce
Mahadev710 <mahadevkeshari790@gmail.com>
2025-10-11 20:24:01 +0530

    Solved symmetrical polygon

--- C_Symmetrical_Polygons.cpp
+++ C_Symmetrical_Polygons.cpp
@@ -7,6 +7,7 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+const int MAXN = 200'005;
 
 #define fastio()                 \
     ios::sync_with_stdio(false); \
@@ -31,7 +32,47 @@ ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
 ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
 
 void solve() {
-   
+   ll n;
+   cin>>n;
+   map<ll,ll> cnt;
+   ll arr[MAXN];
+   for(ll i=0;i<n;i++) cin>>arr[i];
+   for(ll i=0;i<n;i++) cnt[arr[i]]++;
+
+   ll base=0;
+   vector<ll> odd,even;
+   for(auto [x,c] :cnt){
+    //by this we take out max even number for pairing from  (odd as well as even number )frequencies base+=x*(c/2);  
+    base+=x*(c/2);
+    if(c%2==1) odd.push_back(x);
+    else if(c>=2)even.push_back(x);
+   }
+   if(base==0){
+    cout<<0<<endl;
+    return;
+   }
+   ll ans=0;
+   //Checking the 2nd case
+   for(ll x:odd){
+    if(2*base >x) {
+        ans=max(ans,2*base+x);
+    }
+   }
+
+   assert(is_sorted(odd.begin(),odd.end()));
+   // checking the third case
+   for(ll i=1;i< odd.size();i++){
+    if(odd[i-1]+2*base > odd[i]){
+        ans=max(ans,odd[i-1] +2*base +odd[i]);
+    }
+
+   }
+   for(ll x: even){
+    if(base-x >0){
+        ans=max(ans,2*base);
+    }
+   }
+  cout<<ans<<endl;
 }
 
 int main()
