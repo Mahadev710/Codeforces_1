7c27d7c4a2101f203f655d0da266efd86e023420
Mahadev710 <mahadevkeshari790@gmail.com>
2025-10-12 18:56:03 +0530

    solved good string 1500 rating

---
+++ D_a_Good_String.cpp
@@ -0,0 +1,58 @@
+/*
+        ॐ श्रीकृष्णाय नमः ॐ
+        ----------------------------------------
+        |      Author: Mahadev Keshari        |
+        ----------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+using ll = long long;
+#define range(i, a, b) for (auto i = (a); i < (b); ++i)
+#define derange(i, a, b) for (auto i = (a); i >= (b); --i)
+
+bool isPrime(ll n)
+{
+    if (n < 2)
+        return false;
+    if (n % 2 == 0)
+        return n == 2;
+    for (ll i = 3; i * i <= n; i += 2)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+
+ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+ll calc(string s,char c){
+    if(s.size()==1) return s[0] !=c;
+    ll mid=s.size()/2;
+    ll cntl=calc(string(s.begin(),s.begin() +mid),c+1);
+    cntl+=s.size()/2 -count(s.begin()+mid,s.end(),c);
+    ll cntr=calc(string(s.begin()+mid,s.end()),c+1);
+    cntr+=s.size()/2-count(s.begin(),s.begin()+mid,c);
+    return min(cntl,cntr);
+}
+void solve() {
+    ll n;
+    string s;
+    cin>>n>>s;
+    cout<<calc(s,'a')<<endl;
+
+}
+
+int main()
+{
+    fastio();
+    int t = 1;
+    if (!(cin >> t))
+        return 0;
+    while (t--)
+        solve();
+    return 0;
+}
