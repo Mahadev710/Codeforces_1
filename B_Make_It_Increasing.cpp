a9991c1b9a9300fcf3ef63d58db72c02ef2ddf11
Mahadev710 <mahadevkeshari790@gmail.com>
2025-10-18 22:41:31 +0530

    A

---
+++ B_Make_It_Increasing.cpp
@@ -0,0 +1,67 @@
+/*
+        ॐ श्रीकृष्णाय नमः ॐ
+        ----------------------------------------
+        |      Author: Mahadev Keshari        |
+        ----------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+using ll = long long;
+#define range(i, a, b) for (auto i = (a); i < (b); ++i)
+#define derange(i, a, b) for (auto i = (a); i >= (b); --i)
+
+bool isPrime(ll n)
+{
+    if (n < 2)
+        return false;
+    if (n % 2 == 0)
+        return n == 2;
+    for (ll i = 3; i * i <= n; i += 2)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+
+ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+
+bool isPowerofTwo(ll n){
+    return (n>0) && ((n&(n-1))==0);
+}
+
+void solve() {
+   ll n;
+   cin>>n;
+   vector<ll> arr(n);
+   range(i,0,n) cin>>arr[i];
+
+   ll ans=0;
+
+   derange(i,n-2,0) {
+    while(arr[i] >=arr[i+1] and arr[i]>0){
+        arr[i]/=2;
+        ans++;
+    }
+    if(arr[i]==arr[i+1]){
+        cout<<-1<<endl;
+        return;
+    }
+   }
+   cout<<ans<<endl;
+}
+
+int main()
+{
+    fastio();
+    int t = 1;
+    if (!(cin >> t))
+        return 0;
+    while (t--)
+        solve();
+    return 0;
+}
