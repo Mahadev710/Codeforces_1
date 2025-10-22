a9e7ddd65208f2c2e303ee8ccce5d30226de4d68
Mahadev710 <mahadevkeshari790@gmail.com>
2025-10-22 11:30:41 +0530

    Added "

---
+++ B_01_Game.cpp
@@ -0,0 +1,60 @@
+/*
+        ॐ श्रीकृष्णाय नमः ॐ
+        ----------------------------------------
+        |      Author: Mahadev Keshari        |
+        ----------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+using ll = long long;
+#define range(i, a, b) for (auto i = (a); i < (b); ++i)
+#define derange(i, a, b) for (auto i = (a); i >= (b); --i)
+
+bool isPrime(ll n)
+{
+    if (n < 2)
+        return false;
+    if (n % 2 == 0)
+        return n == 2;
+    for (ll i = 3; i * i <= n; i += 2)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+
+ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+
+bool isPowerofTwo(ll n){
+    return (n>0) && ((n&(n-1))==0);
+}
+
+void solve() {
+   string s;
+   cin>>s;
+   ll n=s.size();
+   ll cnt0=0,cnt1=0;
+   for(auto x:s){
+    if(x=='1') cnt1++;
+    else cnt0+=1;
+   }
+   ll mini=min(cnt0,cnt1);
+   if(mini&1) cout<<"DA"<<endl;
+   else cout<<"NET"<<endl;
+}
+
+int main()
+{
+    fastio();
+    int t = 1;
+    if (!(cin >> t))
+        return 0;
+    while (t--)
+        solve();
+    return 0;
+}
---
+++ B_Multiply_by_2_divide_by_6.cpp
@@ -0,0 +1,64 @@
+/*
+        ॐ श्रीकृष्णाय नमः ॐ
+        ----------------------------------------
+        |      Author: Mahadev Keshari        |
+        ----------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+using ll = long long;
+#define range(i, a, b) for (auto i = (a); i < (b); ++i)
+#define derange(i, a, b) for (auto i = (a); i >= (b); --i)
+
+bool isPrime(ll n)
+{
+    if (n < 2)
+        return false;
+    if (n % 2 == 0)
+        return n == 2;
+    for (ll i = 3; i * i <= n; i += 2)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+
+ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+
+bool isPowerofTwo(ll n){
+    return (n>0) && ((n&(n-1))==0);
+}
+
+void solve() {
+   ll n;
+   cin>>n;
+   ll cnt2=0,cnt3=0;
+   while(n%2==0){
+    n/=2;
+    cnt2++;
+   }
+    while(n%3==0){
+    n/=3;
+    cnt3++;
+   }
+   if(n==1 && cnt2<=cnt3){
+      cout<<cnt3+cnt3-cnt2<<endl;
+   }
+   else cout<<-1<<endl;
+}
+
+int main()
+{
+    fastio();
+    int t = 1;
+    if (!(cin >> t))
+        return 0;
+    while (t--)
+        solve();
+    return 0;
+}
